ce89f2da29771b6488361b52c2e8cd46fa1b4f3e
LarryTheKing <lartking@gmail.com>
2015-04-18 14:57:56 -0400

    Gamepad Input Done

--- RoverPC/RoverPC.cpp
+++ RoverPC/RoverPC.cpp
@@ -1,11 +1,32 @@
 // RoverPC.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include "Gamepad.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
+	using namespace RoverPC;
+
+	Gamepad * g = new Gamepad(0);
+
+	// Test Gamepad
+	while (true)
+	{
+		// Clear stream
+		system("cls");
+
+		g->Update();
+		std::cout << g->GetState()._RAW;
+
+		if ((g->GetState().B & GAMEPAD_A) == GAMEPAD_A)
+			break;
+
+		Sleep(100);
+	}
+
+	delete g;
+
 	return 0;
 }
 
